5857d4579b35ac5971cccb1d5e32c347dd8bcdf7
Iker de la Iglesia <ikerprogrammer0610@gmail.com>
2022-06-20 18:08:34 +0200

    Updated Model

--- annotation_visualizer/model/model.py
+++ annotation_visualizer/model/model.py
@@ -1,38 +1,50 @@
 import json
-
-from .json_types import AnnotationDict, AnnotatedTextDict
-
-
-# Data classes
-class Annotation:
-    def __init__(self, annotation: AnnotationDict):
-        self.label: str = annotation['concept_category']
-        self.start_position: int = annotation['start_position']
-        self.end_position: int = annotation['end_position']
+from typing import TypedDict
 
 
-class AnnotatedText:
-    def __init__(self, annotated_text: AnnotatedTextDict):
-        self.note_id: str = str(annotated_text['note_id'])
-        self.annotator: str = annotated_text['task_executor']
-        self.task_result: list[Annotation] = [Annotation(annotation) for annotation in annotated_text['task_result']]
-        self.note_text: str = annotated_text['note_text']
+class DatasetMetrics(TypedDict):
+    Agreement: float
+    BIAS: float
+    KAPPA: float
+    PI: float
 
 
-# Types
-GroupedAnnotatedDataset = dict[str, dict[str, AnnotatedText]]
+class FileMetrics(TypedDict):
+    annotators: list[str]
+    stats: dict[str, int | list]
+    metrics: dict[str, float]
+
+
+class Annotation(TypedDict):
+    id: str
+    entity: str
+    start_position: int
+    end_position: int
+    label: str
+
+
+class NoteAnnotations(TypedDict):
+    annotator: str
+    annotations: list[Annotation]
+
+
+class NoteWithAnnotations(TypedDict):
+    note_id: int
+    filename: str
+    note_text: str
+    metrics: list
+    annotator_annotations: dict[str, NoteAnnotations]
+
+
+GroupedAnnotatedDataset = dict[str, NoteWithAnnotations]
+
+
+class DatasetWithMetrics(TypedDict):
+    dataset_metrics: dict[str, DatasetMetrics]
+    annotated_dataset: GroupedAnnotatedDataset
 
 
 # Functions
-def load_annotations_from_json(json_path: str) -> list[AnnotatedTextDict]:
+def load_annotations_from_json(json_path: str) -> DatasetWithMetrics:
     with open(json_path, encoding='utf-8') as json_file:
         return json.load(json_file)
-
-
-def load_grouped_annotated_dataset(json_path: str) -> GroupedAnnotatedDataset:
-    dataset: GroupedAnnotatedDataset = dict()
-    for annotated_text_dict in load_annotations_from_json(json_path):
-        annotated_text = AnnotatedText(annotated_text_dict)
-        dataset.setdefault(annotated_text.note_id, dict())[annotated_text.annotator] = annotated_text
-
-    return dataset
